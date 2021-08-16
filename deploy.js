d1d3e550ddc1b45677d482ee5880d8ddf3cb2ab0
Matthew Dias <matthewdias@me.com>
2021-08-16 03:30:04 -0500

    More babel

--- deploy.js
+++ deploy.js
@@ -1,20 +1,14 @@
 const fs = require('fs');
 const browserify = require('browserify');
-const errorify = require('errorify');
-const uglifyify = require('uglifyify');
 
 const b = browserify({
 		entries: [ './src/index.js' ],
 		cache: {},
 		packageCache: {},
-		plugin: [ errorify ]
 	})
 	.transform('babelify', {
 		presets: [ '@babel/preset-env', '@babel/preset-react' ]
 	})
-	.transform({
-		global: true
-	}, 'uglifyify');
 
 b.on('update', bundle);
 b.on('log', console.log);
