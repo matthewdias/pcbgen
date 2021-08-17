a40c1156739b1947570e061de14d5f62afb1bcd2
Matthew Dias <matthewdias@me.com>
2021-08-17 00:22:53 -0500

    layout names

--- src/ui/layouts/index.js
+++ src/ui/layouts/index.js
@@ -19,17 +19,19 @@ class Layouts extends React.Component {
 		const keyboard = state.keyboard;
 
 		return <div style={{ display: 'flex', flexDirection: 'column', width: 200 }}>
-			<h4>Layouts&nbsp;&nbsp;&nbsp;</h4>
+			<h4>LAYOUTS&nbsp;&nbsp;&nbsp;</h4>
             {Object.keys(keyboard.layouts).map((layout) => (
-                <select
-                    key={layout}
-                    style={{ width: '10rem', marginBottom: '1rem' }}
-                    value={ state.ui.get(`layout:${layout}`) }
-                    onChange={ e => state.ui.set(`layout:${layout}`, e.target.value)}>
-                    {keyboard.layouts[layout].map((layoutOption) => (
-                        <option key={layoutOption} value={layoutOption}>{layoutOption}</option>
-                    ))}
-                </select>
+                <div key={layout}>
+                    <h4>{layout}</h4>
+                    <select
+                        style={{ width: '10rem', marginBottom: '1rem' }}
+                        value={ state.ui.get(`layout:${layout}`) }
+                        onChange={ e => state.ui.set(`layout:${layout}`, e.target.value)}>
+                        {keyboard.layouts[layout].map((layoutOption) => (
+                            <option key={layoutOption} value={layoutOption}>{layoutOption}</option>
+                        ))}
+                    </select>
+                </div>
             ))}
 			<br/><br/>
 		</div>;
