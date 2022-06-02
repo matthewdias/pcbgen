44f9f3ccf57de2bdfdf51826e7dd245f0bea5a75
Francis Usher <francis.w.usher@gmail.com>
2022-06-01 23:07:46 -0700

    chore: configure-out some dumb lint/type errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "airbnb",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
@@ -19,5 +24,6 @@ module.exports = {
       2,
       { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
+    "react/react-in-jsx-scope": "off",
   },
 }
